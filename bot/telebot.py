350257deb4dc4dcea8ad9ef9f5160f8bcbcff090
BackSoul <danielsarmientodeveloper@gmail.com>
2021-08-14 19:53:34 -0500

    :memo: env variables python docker
    passing env python mysql

--- bot/telebot.py
+++ bot/telebot.py
@@ -5,9 +5,15 @@ from telegram import Update
 import emoji
 from dotenv import load_dotenv
 import os
+import mysql.connector
+
 load_dotenv()  # take environment variables from .env.
 def start(update, context):
+    print("Iniciaron el bot!!")
     update.message.reply_text(f'Hello {update.effective_user.first_name} :pulgar_hacia_arriba:')
+    update.message.reply_text(os.environ["DB_HOST"])
+    update.message.reply_text(os.environ["DB_USER"])
+    update.message.reply_text(os.environ["DB_PASSWORD"])
  
 
 def main():
@@ -17,6 +23,13 @@ def main():
     dp.add_handler(CommandHandler('start',	start))
     updater.start_polling()
     updater.idle()
+    
 
 if __name__ == '__main__':
+    # mydb = mysql.connector.connect(
+    # host=os.environ["DB_HOST"],
+    # user=os.environ["DB_USER"],
+    # password=os.environ["DB_PASSWORD"]
+    # )
+    # print(mydb)
     main()
